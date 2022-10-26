bfeda2519b42de67ba108a6b475110fc51abbfe7
Sekai Alien <sekai.alien@mail.ru>
2022-10-26 21:07:37 +0500

    task 23 - cube

---
+++ task-23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int cube = 1;
+while(cube <= N)
+{
+    Console.Write(Math.Pow(cube, 3));
+    cube++;
+    if(cube <= N)
+    {
+        Console.Write(", ");
+    }
+}
